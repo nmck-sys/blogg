b1588a082d9497e0b9090aaf93f15bd3e9c163ce
HA_GO\antic <nmckinzie@smcm.edu>
2024-07-25 23:02:22 -0500

    js/text

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Noah's Portfolio
+
+## Description
+
+This page was created to provide a format for blog creation. Using this page, we can enter data into the text boxes are create our own blog. 
+
+## Table of Contents
+
+Screenshot:
+
+![Screenshot of deployed application](Screenshot.png)
+
+Link to application:
+
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,12 +8,46 @@ document.addEventListener('DOMContentLoaded', function() {
         const title = document.querySelector('#blog-title').value;
         const content = document.querySelector('#blog-content').value;
 
-        // Save the data above into localStorage
         localStorage.setItem('blogAuthor',author);
         localStorage.setItem('blogTitle',title);
         localStorage.setItem('blogContent',content);
 
-        // redirect to blog.html
-        window.location.href = 'blog.html'
+
+        document.querySelectorAll('.error').forEach(el => el.remove());
+
+
+let isValid = true;
+
+        if (!author) {
+            showError('#blog-author', 'Author is required');
+            isValid = false;
+        }
+
+        if (!title) {
+            showError('#blog-title', 'Title is required');
+            isValid = false;
+        }
+
+        if (!content) {
+            showError('#blog-content', 'Content is required');
+            isValid = false;
+        }
+
+        if (isValid) {
+            localStorage.setItem('blogAuthor', author);
+            localStorage.setItem('blogTitle', title);
+            localStorage.setItem('blogContent', content);
+
+            window.location.href = 'blog.html';
+        }
     });
+
+    function showError(selector, message) {
+        const inputElement = document.querySelector(selector);
+        const errorElement = document.createElement('div');
+        errorElement.className = 'error';
+        errorElement.style.color = 'red';
+        errorElement.textContent = message;
+        inputElement.parentNode.insertBefore(errorElement, inputElement.nextSibling);
+    }
 });
